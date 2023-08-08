3b2c46d60821220d695b55424ec05f9a68346720
JD <joyjitdas26@gmail.com>
2023-08-09 01:49:11 +0530

    Project setup commit 2

--- backend/app.js
+++ backend/app.js
@@ -12,14 +12,14 @@ app.use(express.json());
 app.use(cors(corsOptions));
 
 // connect MongoDB
-mongoose.connect(process.env.MONGODB_URI).then(() => {
+// mongoose.connect(process.env.MONGODB_URI).then(() => {
     const PORT = process.env.PORT || 8000
     app.listen(PORT, () => {
         console.log(`App is Listening on PORT ${PORT}`);
     })
-}).catch(err => {
-    console.log(err);
-});
+// }).catch(err => {
+//     console.log(err);
+// });
 
 // route
 app.get("/", (req, res) => {
